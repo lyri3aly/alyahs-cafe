2727e785ae81c0559ce994fb8d73733e10a68ce6
lyla-e <roseevans498@gmail.com>
2026-06-02 09:14:00 -0400

    setup

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("button");
+const secondbutton = document.getElementById("secondbutton");
+
+button.addEventListener("click", () => {
+    alert("you found me!")
+});
+
+secondbutton.addEventListener("click", () => {
+    alert("woah this is so cool")
+})
