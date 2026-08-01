45e771e3d49cab6f5f72c93444e81f28a3654b8b
lyla-e <roseevans498@gmail.com>
2026-08-01 19:23:26 -0400

    added more elements from my figma design, stars, and cafe menu items

---
+++ script.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll("section, header");
+const navLinks = document.querySelectorAll(".topnav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 500;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.id;
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+
+});
